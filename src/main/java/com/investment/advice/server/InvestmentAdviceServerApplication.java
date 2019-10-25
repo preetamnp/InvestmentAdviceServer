c4c3d91f90539a5ad1a8f0e0da410bd1fb7ee2ae
preetam potadar <preetamnp@gmail.com>
2019-10-25 07:44:37 +0200

    changed the package structure

---
+++ src/main/java/com/investment/advice/server/InvestmentAdviceServerApplication.java
@@ -0,0 +1,13 @@
+package com.investment.advice.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentAdviceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentAdviceServerApplication.class, args);
+	}
+
+}
