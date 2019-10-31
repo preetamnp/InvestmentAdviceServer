230f43894f0fdc7328eaa363b755530e482f0f1e
preetam potadar <preetamnp@gmail.com>
2019-10-31 12:18:32 +0100

    Added Overrider Annotation, so that any changes to swagger will make code fail

--- src/main/java/com/investment/advice/server/controllers/AdviceInvestmentController.java
+++ src/main/java/com/investment/advice/server/controllers/AdviceInvestmentController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+
 import java.util.List;
 
 @Controller
@@ -15,6 +16,8 @@ public class AdviceInvestmentController implements AdviceApi {
     @Autowired
     private AdviceInvestmentService adviceInvestmentService;
 
+
+    @Override
     public ResponseEntity<List<Asset>> getAdvice (Integer riskLevel, Boolean esg) {
         return new ResponseEntity<>(adviceInvestmentService.getAssets(riskLevel,esg), HttpStatus.OK);
     }
